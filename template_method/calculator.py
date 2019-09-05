20f96f098817a6713a95c14a2d2528198ac30765
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-09-05 11:44:59 +0530

    Implemented an abstract base class for an average calculator

---
+++ template_method/calculator.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+
+class AverageCalculator(ABC): 
+
+    def average(self): 
+        try:
+            num_items = 0
+            total_sum = 0
+            while self.has_next():
+                total_sum += self.next_item()
+                num_items += 1
+            if num_items == 0:
+                raise RuntimeError("Can't compute the average of zero items.")
+            return total_sum / num_items
+        finally:
+            self.dispose()
+
+    @abstractmethod
+    def has_next(self): 
+        pass
+
+    @abstractmethod
+    def next_item(self): 
+        pass
+
+    def dispose(self): 
+        pass

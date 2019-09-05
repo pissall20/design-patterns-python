ea1fd048161c93e3c699224cd1bfb7fb8e6a28d8
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-09-05 11:58:59 +0530

    Added a class that inherits from AverageCalculator to read numbers from file and print the average

--- template_method/calculator.py
+++ template_method/calculator.py
@@ -26,3 +26,26 @@ class AverageCalculator(ABC):
 
     def dispose(self): 
         pass
+    
+
+class FileAverageCalculator(AverageCalculator):
+
+    def __init__(self, file): 
+        self.file = file
+        self.last_line = self.file.readline() 
+
+    def has_next(self):
+        return self.last_line != '' 
+
+    def next_item(self):
+        print(self.last_line)
+        result = float(self.last_line)
+        self.last_line = self.file.readline() 
+        return result
+
+    def dispose(self):
+        self.file.close()
+
+fac = FileAverageCalculator(open('data.txt'))
+print(fac.average()) # Call the template method
+

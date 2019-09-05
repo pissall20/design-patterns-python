7531e73e2cae923d581a2046dd96d2ef9d8177d9
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-09-05 15:24:06 +0530

    Added comments for adapter pattern

--- adapter/poultry.py
+++ adapter/poultry.py
@@ -15,6 +15,17 @@ class Turkey:
     def fly(self):
         print("I'm flying a short distance")
 
+
+"""
+You want your turkeys to behave like ducks, so you need to apply the Adapter pattern. In the same file, write a class called TurkeyAdapter and make sure it takes into account the following:
+
+The adapter’s __init__ method should take its adaptee as an argument.
+
+The quack translation between classes is easy: just call the gobble method when appropriate.
+
+Even though both classes have a fly method, turkeys can only fly in short spurts — they can’t do long-distance flying like ducks. To map between a duck’s fly method and the turkey’s method, you need to call the turkey’s fly method five times to make up for it.
+"""
+
 class TurkeyAdapter:
 
     def __init__(self, adaptee):
@@ -26,6 +37,8 @@ class TurkeyAdapter:
     def fly(self):
         for i in range(5):
             self.adaptee.fly()
+
+# Test with the following code:
 
 def duck_interaction(duck):
     duck.quack()

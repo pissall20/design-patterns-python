b0a38bd71d14559cafa9f1a9fcccb1e8968004c8
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2019-09-05 11:42:52 +0530

    Made Tigger class private and interfaced with Tigger function, to return a singleton

--- singleton/tigger.py
+++ singleton/tigger.py
@@ -1,8 +1,16 @@
-class Tigger:
+class _Tigger:
 
     def __str__(self):
         return "I'm the only one!"
 
     def roar(self):
         return 'Grrr!'
+
+_instance = None
+
+def Tigger():
+    global _instance
+    if not _instance:
+        _instance = _Tigger()
+    return _instance
 
